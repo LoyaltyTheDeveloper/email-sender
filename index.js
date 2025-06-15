aeb9070388a0ed725ad1fe160f7798f940b15e07
Loyalty <lohunmule@gmail.com>
2025-06-15 01:49:21 +0100

    addition

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT 
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
